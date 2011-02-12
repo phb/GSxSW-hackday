f044f42b6476920e4bf59539c3b665b9171179e8
Johan Bjork <phb@spotify.com>
2011-02-12 21:44:13 +0100

    add USE_GTK

--- opengazer-0.1.2/opengazer.cpp
+++ opengazer-0.1.2/opengazer.cpp
@@ -1,11 +1,14 @@
 #include <gtkmm.h>
 #include <iostream>
-#include "GazeTrackerGtk.h"
 #include "OutputMethods.h"
+#if USE_GTK
+#include "GazeTrackerGtk.h"
 #include "GtkStore.h"
+#endif
 
 int main(int argc, char **argv)
 {
+#if USE_GTK
     Gtk::Main kit(argc, argv);
     Glib::thread_init();
 
@@ -17,6 +20,9 @@ int main(int argc, char **argv)
     helloworld.show();
 
     Gtk::Main::run(helloworld);
+#else 
+#warning implement me!!
+#endif
     
     return 0;
 }
