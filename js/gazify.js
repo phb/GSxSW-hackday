95b8f0586b23399d53731ef2ff2d67f0c875ecf2
Felix Bruns <felixbruns@spotify.com>
2011-02-13 12:18:40 +0000

    Fix document.elementFromScreenPoint etc.

--- js/gazify.js
+++ js/gazify.js
@@ -1,72 +1,68 @@
-function consolelog(text) {
-    document.getElementById("dbg").innerHTML = text
-    console.log(text)
-}
-function elementFromScreenPoint(x, y) {
-	x -= window.screenX;
-	y -= window.screenY;
-	//y += document.body.scrollTop;
-	//y -= 96;
-	y -= 22;
-	var gazemarker = document.getElementById("gazemarker");
-	document.body.removeChild(gazemarker)
-	el = document.elementFromPoint(x, y);
-	document.body.appendChild(gazemarker)
-	y += document.body.scrollTop;
-/*	if(!el)
-		consolelog("did not Found el!")*/
-	//consolelog("doc location supposedly " + x + ", " + y)
-	gazemarker.style.top = y+"px";
-	gazemarker.style.left = x+"px";
-	return el;
-}
-document.addEventListener("mousemove", function(e) {
-	document.gaze(e.pageX, e.pageY);
-});
-
-var currentElement = null;
-var currentTrack = null;
-var contestantElement = null;
-var contestantUnderGazeSince = 0;
-function setCurrentElement(el) {
-    if(el == currentElement) return;
-    consolelog("current element is now " + el);
-    if(currentElement) currentElement.style.backgroundColor = "white"
-    currentElement = el;
-    currentTrack = el ? el.getAttribute("gazemusic") : null;
-    if(el) {
-        el.style.backgroundColor = "green";
-        plugin().playTrack(currentTrack);
-    } // else pause
-}
-function setContestantElement(el) {
-    if(el == currentElement) return;
-    
-    var now = (new Date).getTime();
-    if(el == contestantElement) {
-        if(now - contestantUnderGazeSince > 2000) {
-            setCurrentElement(contestantElement);
-            contestantElement = null;
-            contestantUnderGazeSince = 0;
-        }
-        return;
-    } else {
-        if(contestantElement) contestantElement.style.backgroundColor = "white";
-        contestantElement = el;
-        contestantUnderGazeSince = now;
-        if(contestantElement) contestantElement.style.backgroundColor = "yellow";
-    }
+var isChrome = function() {
+	return navigator.userAgent.indexOf('Chrome') != -1;
 }
 
-document.gaze = function(x,y) {
-	var el = elementFromScreenPoint(x, y);
-    while(el != null && el != document) {
-        var track = el.getAttribute("gazemusic");
-        if(track && track.length > 0) {
-            break;
-        }
-		el = el.parentNode;
+var isSafari = function() {
+	return navigator.vendor.indexOf('Apple') != -1;
+}
+
+var isFirefox = function() {
+	return navigator.userAgent.indexOf('Firefox') != -1;
+}
+
+document.elementFromScreenPoint = function(x, y) {
+	var offX = (window.screenX < 0) ? 0 : window.screenX;
+	var offY = (window.screenY < 0) ? 0 : window.screenY;
+
+	if (isChrome()) {
+		x -= offX;
+		y -= offY + 60;
+	}
+	else if (isSafari()) {
+		x -= offX;
+		y -= offY + 96;
+	}
+	else if (isFirefox()) {
+		x -= window.mozInnerScreenX;
+		y -= window.mozInnerScreenY;
+	}
+	else {
+		x -= offX;
+		y -= offY;
+	}
+
+	return document.elementFromPoint(x, y);
+}
+
+document.addEventListener("click", function(e) {
+	console.log(e.screenY);
+
+	document.gaze(e.clientX, e.clientY);
+}, true);
+
+var oldElement = null;
+
+document.gaze = function(x, y) {
+	var marker = document.getElementById('gazemarker');
+	var e      = document.elementFromPoint(x, y);
+	var uri    = null;
+
+	marker.style.top  = y + 'px';
+	marker.style.left = x + 'px';
+
+	if(oldElement)
+		oldElement.style.backgroundColor =  'white';
+
+    while(e != undefined && e != document && (uri = e.getAttribute('gazemusic')) == null){
+		e = e.parentNode;
     }
-    if(el == document) el = null;
-    setContestantElement(el);
+
+	if(uri != null){
+		console.log(uri);
+		plugin().playTrack(uri);
+
+		e.style.backgroundColor = 'red';
+
+		oldElement = e;
+	}
 }
